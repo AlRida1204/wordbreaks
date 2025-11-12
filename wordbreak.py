ccb410ef116a2d5d8e292a54cf4669bd04049b45
AlRida1204 <165802031+AlRida1204@users.noreply.github.com>
2025-11-12 17:36:25 +0530

    wordbreak.py

--- wordbreak.py
+++ wordbreak.py
@@ -4,9 +4,7 @@ def wordBreak(s, wordDict):
     dp = [False] * (n + 1)
     dp[0] = True
     for i in range(1, n + 1):
-        for j in range(i):
-            if dp[j] and s[j:i] in word_set:
-                dp[i] = TrueS
+        for j in range(i):
                 break
     return dp[n]
 s = input("Enter a string: ")
